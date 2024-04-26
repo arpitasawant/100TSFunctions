da38f1ebb6679b498a0c9005f1abf518e1d68a56
arpita.sawant <arpita.sawant@coditas.com>
2024-04-26 13:38:45 +0530

    concat three strings

---
+++ 03.concat3.ts
@@ -0,0 +1,5 @@
+function concat3(string1:string, string2:string, string3:string, separator:string):string {
+    return `${string1}${separator}${string2}${separator}${string3}`
+}
+
+export { concat3 };
